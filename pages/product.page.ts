2d2832e3dfb0d19f3cc442a5ba2afd591b7ede80
Hiep Luong LM-Apps <hiep.luong@aa.com>
2023-12-21 18:55:50 -0700

    update Product page & steps

--- pages/product.page.ts
+++ pages/product.page.ts
@@ -3,6 +3,7 @@ import { Page } from "@playwright/test"
 export class Product {
     private readonly page: Page
     private readonly addToCart: string = 'button[id="add-to-cart-sauce-labs-backpack"]'
+    private readonly cartBtn: string = '[id="shopping_cart_container"]';
 
     constructor(page: Page) {
         this.page = page;
@@ -10,5 +11,7 @@ export class Product {
 
     public async addBackPackToCart() {
         await this.page.locator(this.addToCart).click()
+    }
+    public async selectCart() {
     }
 }
--- steps/product.steps.ts
+++ steps/product.steps.ts
@@ -1,7 +1,12 @@
 import { Then } from '@cucumber/cucumber';
 import { getPage } from '../playwrightUtilities';
 import { Product } from '../pages/product.page';
+import { Checkout } from '../pages/checkout.page';
 
 Then('I will add the backpack to the cart', async () => {
   await new Product(getPage()).addBackPackToCart();
+});
+Then('I can view cart', async () => {
+  await new Product(getPage()).
+  await new Checkout(getPage().checkout
 });
