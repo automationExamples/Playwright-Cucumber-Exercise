12adc7949a16cef256482084afe0196749ba415d
Maheshwari Allam <maheshwariallam@Maheshwaris-MacBook-Pro.local>
2023-12-22 10:29:30 -0500

    Scripts added

--- pages/product.page.ts
+++ pages/product.page.ts
@@ -3,6 +3,9 @@ import { Page } from "@playwright/test"
 export class Product {
     private readonly page: Page
     private readonly addToCart: string = 'button[id="add-to-cart-sauce-labs-backpack"]'
+    private readonly cartButton: string = '//div[@id="shopping_cart_container"]'
+    private readonly menuButton: string = '//button[contains(@id,"menu-btn")]'
+    private readonly logoutButton: string = '//a[@id="logout_sidebar_link"]'
 
     constructor(page: Page) {
         this.page = page;
@@ -10,5 +13,14 @@ export class Product {
 
     public async addBackPackToCart() {
         await this.page.locator(this.addToCart).click()
+    }
+
+    public async clickOnCartButton(){
+        await this.page.locator(this.cartButton).click()
+    }
+
+    public async clickOnLogout(){
+        await this.page.locator(this.menuButton).click()
+        await this.page.locator(this.logoutButton).click()
     }
 }
