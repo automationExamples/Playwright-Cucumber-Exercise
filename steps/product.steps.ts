d02eb2322a7a8c57eb233dfdf27cfde6a4aa48a1
Dhatravani Haresamudra <34672365+dhatravani@users.noreply.github.com>
2024-02-16 11:20:19 -0600

    Update product.steps.ts

--- steps/product.steps.ts
+++ steps/product.steps.ts
@@ -1,7 +1,20 @@
-import { Then } from '@cucumber/cucumber';
+import { Then, When } from '@cucumber/cucumber';
 import { getPage } from '../playwrightUtilities';
 import { Product } from '../pages/product.page';
 
-Then('I will add the backpack to the cart', async () => {
+Then('Add the backpack to the cart', async () => {
   await new Product(getPage()).addBackPackToCart();
+});
+
+When('Click on Cart', async () => {
+  await new Product(getPage()).clickOnCartButton();
+});
+
+When('Sort the items by {string}', async (sortName) => {
+  await new Product(getPage()).selectSortOption(sortName);
+});
+
+Then('Validate all 6 items are sorted correctly by {string}', async(sortName) => {
+  const listOfProducts = await new Product(getPage()).getListOfProductsByPriceSort(sortName);
+  
 });
