db4b1bb15f5b2ad0407f69743ccea2cafa985c2e
Wesley Wu <wjwu1996@gmail.com>
2024-07-01 19:50:05 -0400

    fix second test

--- pages/login.page.ts
+++ pages/login.page.ts
@@ -6,6 +6,7 @@ export class Login {
     private readonly passwordField: string = 'input[id="password"]'
     private readonly userNameField: string = 'input[id="user-name"]'
     private readonly loginButton: string = 'input[id="login-button"]'
+    private readonly errorBox: string = 'h3[data-test="error"]';
 
     constructor(page: Page) {
         this.page = page;
@@ -14,7 +15,7 @@ export class Login {
     public async validateTitle(expectedTitle: string) {
         const pageTitle = await this.page.title();
         if (pageTitle !== expectedTitle) {
-          throw new Error(`Expected title to be ${expectedTitle} but found ${pageTitle}`);
+            throw new Error(`Expected title to be ${expectedTitle} but found ${pageTitle}`);
         }
     }
 
@@ -22,5 +23,12 @@ export class Login {
         await this.page.locator(this.userNameField).fill(userName)
         await this.page.locator(this.passwordField).fill(this.password)
         await this.page.locator(this.loginButton).click()
+    }
+
+    public async validateErrorMessage(expectedErrorMessage: string) {
+        const receivedError = await this.page.locator(this.errorBox).innerText();
+        if (receivedError != expectedErrorMessage) {
+            throw new Error(`Expected the error message to be ${expectedErrorMessage} but found ${receivedError}`);
+        }
     }
 }
