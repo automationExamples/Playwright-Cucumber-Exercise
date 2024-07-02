2886a6744eca03ab245ed8b3c432ae67a2e35201
Wesley Wu <wjwu1996@gmail.com>
2024-07-01 20:11:58 -0400

    fix third test

--- features/purchase.feature
+++ features/purchase.feature
@@ -7,8 +7,13 @@ Feature: Purchase Feature
   Then I will login as 'standard_user'
   Then I will add the backpack to the cart
     # TODO: Select the cart (top-right)
+  Then I will click on the cart icon
     # TODO: Select Checkout
+  Then I will click on the checkout button
     # TODO: Fill in the First Name, Last Name, and Zip/Postal Code
+  Then I will fill in my shipping information
     # TODO: Select Continue
     # TODO: Select Finish
+  Then I will confirm my purchase
     # TODO: Validate the text 'Thank you for your order!'
+  Then I should see the message "Thank you for your order!"
--- steps/product.steps.ts
+++ steps/product.steps.ts
@@ -5,3 +5,23 @@ import { Product } from '../pages/product.page';
 Then('I will add the backpack to the cart', async () => {
   await new Product(getPage()).addBackPackToCart();
 });
+
+Then('I will click on the cart icon', async () => {
+  await new Product(getPage()).selectCartIcon();
+});
+
+Then('I will click on the checkout button', async () => {
+  await new Product(getPage()).selectCheckout();
+});
+
+Then('I will fill in my shipping information', async () => {
+  await new Product(getPage()).fillInShipping();
+});
+
+Then('I will confirm my purchase', async () => {
+  await new Product(getPage()).confirmPurchase();
+});
+
+Then('I should see the message {string}', async (expectedMessage) => {
+  await new Product(getPage()).validatePurchaseMessage(expectedMessage);
+});
