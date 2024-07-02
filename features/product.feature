07a748b6465a58a6ac005efdf1a4b09081480761
archana8578 <archana.8578@gmail.com>
2024-07-02 16:50:54 -0400

    Test completed - Archana Akella

--- features/product.feature
+++ features/product.feature
@@ -6,8 +6,12 @@ Feature: Product Feature
   # Create a datatable to validate the Price (high to low) and Price (low to high) sort options (top-right) using a Scenario Outline
   Scenario Outline:  Validate product sort by price <sort>
   Then I will login as 'standard_user'
+  Then Sort the items by '<sort>'
+  Then Validate all 6 items are sorted correctly by '<sort>'
     # TODO: Sort the items by <sort>
     # TODO: Validate all 6 items are sorted correctly by price
   Examples:
     # TODO: extend the datatable to paramterize this test
     | sort |
+    |Price (low to high)|
+    |Price (high to low)|
--- steps/product.steps.ts
+++ steps/product.steps.ts
@@ -1,7 +1,46 @@
 import { Then } from '@cucumber/cucumber';
 import { getPage } from '../playwrightUtilities';
 import { Product } from '../pages/product.page';
+import { purchase } from '../pages/purchase.page';
 
 Then('I will add the backpack to the cart', async () => {
   await new Product(getPage()).addBackPackToCart();
 });
+
+Then('Select the cart \\(top-right)', async () => {
+  await new purchase(getPage()).selectCart();
+});
+
+Then('Select Checkout', async () => {
+  await new purchase(getPage()).clickCheckout();
+
+});
+
+Then('Fill in the firstname as {string} lastname as {string} and postcode as {string}', async (firstName, lastName, zipCode) => {
+  await new purchase(getPage()).fillCheckoutInformation(firstName, lastName, zipCode);
+});
+
+Then('Select Continue', async () => {
+  await new purchase(getPage()).clickOnContinue();
+});
+
+Then('Select Finish', async () => {
+  await new purchase(getPage()).clickOnFinish();
+});
+
+
+Then('Validate the text {string}', async (thankYouMsg) => {
+  await new purchase(getPage()).verifyThankYouMsg(thankYouMsg);
+});
+
+
+
+Then('Sort the items by {string}', async (sortBy) => {
+  await new Product(getPage()).applyFilter(sortBy);
+});
+
+
+Then('Validate all {int} items are sorted correctly by {string}', async (int, sortBy) => {
+  await new Product(getPage()).verifySorting(sortBy);
+});
+
