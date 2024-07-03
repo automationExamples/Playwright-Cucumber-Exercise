d3607208655a1f43accbe83963b080f743f6264a
srirekhamor <srirekhamor@gmail.com>
2024-07-03 11:11:32 -0500

    cucumber exercise

--- pages/login.page.ts
+++ pages/login.page.ts
@@ -1,4 +1,4 @@
-import { Page } from "@playwright/test"
+import { Page, expect } from "@playwright/test"
 
 export class Login {
     private readonly page: Page
@@ -22,5 +22,11 @@ export class Login {
         await this.page.locator(this.userNameField).fill(userName)
         await this.page.locator(this.passwordField).fill(this.password)
         await this.page.locator(this.loginButton).click()
+    }
+
+    public async validateErrorMessage(errorMessage: string) {
+        await this.page.locator('h3').textContent().then((value) => {
+            expect(value).toBe(errorMessage)
+        })
     }
 }
--- steps/product.steps.ts
+++ steps/product.steps.ts
@@ -1,7 +1,42 @@
 import { Then } from '@cucumber/cucumber';
-import { getPage } from '../playwrightUtilities';
 import { Product } from '../pages/product.page';
+import { getPage } from '../playwrightUtilities';
 
 Then('I will add the backpack to the cart', async () => {
   await new Product(getPage()).addBackPackToCart();
+});
+Then('I select the cart', async () => {
+  await new Product(getPage()).selectCart();
+});
+
+Then('I select Checkout', async () => {
+  await new Product(getPage()).selectCheckout();
+});
+
+Then('I fill in the First Name Last Name and Zip Code', async () => {
+  await new Product(getPage()).fillInNameAndPostalCode();
+});
+
+Then('I select Continue', async () => {
+  await new Product(getPage()).selectContinue();
+});
+
+Then('I select Finish', async () => {
+  await new Product(getPage()).selectFinish();
+});
+
+Then('I should validate the text {string}', async (text: string) => {
+  await new Product(getPage()).validateText(text);
+});
+
+Then('I sort the products by {string}', async (sort: string) => {
+  await new Product(getPage()).sortItemsByPrice(sort);
+});
+
+Then('wait for the page to load', async () => {
+  await new Product(getPage()).validateText('Products');
+});
+
+Then('I will validate all 6 items are sorted correctly by price', async () => {
+  await new Product(getPage()).validateSortedItems();
 });
