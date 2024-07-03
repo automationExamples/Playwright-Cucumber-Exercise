aeee4505227a9b2c00cc9cb25d717c1044bd6c99
Swathy Katkuri <Swathy.Katkuri88@gmail.com>
2024-07-02 23:46:14 -0400

    I have completed all the below task Modify the scenario 'Validate the login page title' from login.feature which runs but fails. Determine the cause of the failure and update the scenario to pass in the test  Extend the scenario 'Validate login error message' from login.feature which runs and passes but is missing a step. Extend the scenario to validate the error message received.  Modify and extend the 'Validate successful purchase text' from purchase.feature with steps for each comment listed. Consider writing a new steps.ts file along with an appropriate page.ts  Modify and extend the 'Validate product sort by price sort' from product.feature with steps for each comment listed. Utilize the Scenario Outline and Examples table to parameterize the test  Extend the testing coverage with anything you believe would be beneficial

--- pages/product.page.ts
+++ pages/product.page.ts
@@ -1,8 +1,10 @@
-import { Page } from "@playwright/test"
+import { Page, expect } from "@playwright/test"
+
 
 export class Product {
     private readonly page: Page
     private readonly addToCart: string = 'button[id="add-to-cart-sauce-labs-backpack"]'
+    private readonly filter: string ='select.product_sort_container'
 
     constructor(page: Page) {
         this.page = page;
@@ -10,5 +12,30 @@ export class Product {
 
     public async addBackPackToCart() {
         await this.page.locator(this.addToCart).click()
+    }
+ 
+    public async applyFilter(sort: string) {
+        await this.page.locator(this.filter).selectOption(sort)
+
+    }
+
+    public async verifySorting(sortType: string) {
+        const itemPrices = await this.page.$$eval('.inventory_item_price', elements =>
+            elements.map(element => parseFloat(element.textContent?.replace('$', ' ').trim() || '0'))
+
+        );
+
+        let sortedItemPrices: number[] = [];
+
+        if (sortType === 'Price (low to high)') {
+            // Create a sorted copy of the item prices array
+            sortedItemPrices = [...itemPrices].sort((a, b) => a - b);
+        } else if (sortType === 'Price (high to low)') {
+            // Create a sorted copy of the item prices array
+            sortedItemPrices = [...itemPrices].sort((a, b) => b - a);
+        }
+
+        // Compare the original array with the sorted array
+        expect(itemPrices).toEqual(sortedItemPrices);
     }
 }
