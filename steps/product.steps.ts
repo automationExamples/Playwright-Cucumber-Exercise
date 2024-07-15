9a339f383062cddb40c2478fcbc622fd5eb5afa0
akanthed <akshay.kanthed007@gmail.com>
2024-07-15 23:50:54 +0530

    Hi, I have added solution to all question. Please check and email me akshay.kanthed007@gmail.com

--- steps/product.steps.ts
+++ steps/product.steps.ts
@@ -1,7 +1,15 @@
-import { Then } from '@cucumber/cucumber';
+import { When, Then } from '@cucumber/cucumber';
 import { getPage } from '../playwrightUtilities';
 import { Product } from '../pages/product.page';
 
-Then('I will add the backpack to the cart', async () => {
+When('I will add the backpack to the cart', async () => {
   await new Product(getPage()).addBackPackToCart();
+});
+
+When('I sort products by {string}', async (sort) => {
+  await new Product(getPage()).sortProduct(sort);
+});
+
+Then('all products should be sorted by price {string}', async (sortOrder) => {
+  await new Product(getPage()).validateSort(sortOrder);
 });
