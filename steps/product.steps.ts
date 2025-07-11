62fc3a0bbbfa1db62190f72b756e486e1809cbaa
AdharshaT <adharshathummala@gmail.com>
2025-07-11 16:06:08 -0400

    Task 4: Final fix for dropdown visibility with dynamic wait and logging

--- steps/product.steps.ts
+++ steps/product.steps.ts
@@ -3,5 +3,21 @@ import { getPage } from '../playwrightUtilities';
 import { Product } from '../pages/product.page';
 
 Then('I will add the backpack to the cart', async () => {
-  await new Product(getPage()).addBackPackToCart();
+  const productPage = new Product(getPage());
+  await productPage.addBackPackToCart();
+});
+
+Then('I sort products by {string}', async (sortOption: string) => {
+  const productPage = new Product(getPage());
+  await productPage.sortByOption(sortOption);
+});
+
+Then('the first product displayed should be {string}', async (expectedName: string) => {
+  const productPage = new Product(getPage());
+  await productPage.validateFirstProduct(expectedName);
+});
+
+Then('the last product displayed should be {string}', async (expectedName: string) => {
+  const productPage = new Product(getPage());
+  await productPage.validateLastProduct(expectedName);
 });
