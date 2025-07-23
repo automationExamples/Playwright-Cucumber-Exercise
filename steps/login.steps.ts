12190426f1b6a093ba6abca324702ff964ad8275
Sindhu1302 <157327072+Sindhu1302@users.noreply.github.com>
2025-07-22 22:45:18 -0400

    Cucumber Test - Playwright

--- steps/login.steps.ts
+++ steps/login.steps.ts
@@ -1,6 +1,8 @@
 import { Then } from '@cucumber/cucumber';
 import { getPage } from '../playwrightUtilities';
 import { Login } from '../pages/login.page';
+import { expect, Page } from '@playwright/test';
+
 
 Then('I should see the title {string}', async (expectedTitle) => {
   await new Login(getPage()).validateTitle(expectedTitle);
@@ -8,4 +10,19 @@ Then('I should see the title {string}', async (expectedTitle) => {
 
 Then('I will login as {string}', async (userName) => {
   await new Login(getPage()).loginAsUser(userName);
+});
+
+Then('the error message should be {string}', async function (expectedMessage) {
+  const errorLocator = await new Login(getPage()).getErrorMessage();
+   // Wait until visible
+  await errorLocator.waitFor({ state: 'visible', timeout: 5000 });
+  const actualMessage = await errorLocator.textContent();
+  expect(actualMessage?.trim()).toBe(expectedMessage);
+});
+
+Then('I should see the title header as {string}', async (product) => {
+  const productHeader = await new Login(getPage()).getProductsHeader();
+  await productHeader.waitFor({ state: 'visible', timeout: 5000 });
+  const actualMessage = await productHeader.textContent();
+  expect(actualMessage?.trim()).toBe(product);
 });
