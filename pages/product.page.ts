b6947725330b7e1096895fad4e8e93bbb038f325
Santosh Kumar <skompelli9@gmail.com>
2025-08-11 15:50:33 -0400

    playwright cucumber test fixed

--- pages/product.page.ts
+++ pages/product.page.ts
@@ -1,8 +1,10 @@
-import { Page } from "@playwright/test"
+import { Page, expect } from "@playwright/test"
 
 export class Product {
     private readonly page: Page
     private readonly addToCart: string = 'button[id="add-to-cart-sauce-labs-backpack"]'
+    private readonly sortDropdown: string = '[data-test="product-sort-container"]'
+    private readonly priceSelector: string = '.inventory_item_price'
 
     constructor(page: Page) {
         this.page = page;
@@ -11,4 +13,30 @@ export class Product {
     public async addBackPackToCart() {
         await this.page.locator(this.addToCart).click()
     }
+
+    public async sortItems(sortType: string) {
+        await this.page.selectOption(this.sortDropdown, {label: sortType})
+    }
+
+    public async verifyPriceIsSorted(sortKey: 'lohi' | 'hilo') {
+        const priceTexts = await this.page.locator(this.priceSelector).allTextContents()
+        const prices = priceTexts.map(text => parseFloat(text.replace('$', '')))
+        let expected : number[]
+
+        if (sortKey === 'lohi') {
+            expected = [...prices].sort((a, b) => a - b)
+        } else if (sortKey === 'hilo') {
+            expected = [...prices].sort((a, b) => b - a)    
+        } else {
+            throw new Error('Unsupported sort key ${sortkey}')
+        }
+        expect(prices).toEqual(expected)
+    }
+
+    public async validatePrices() {
+        const selectedType = await this.page.locator(this.sortDropdown).inputValue()
+        await this.verifyPriceIsSorted(selectedType as 'lohi' | 'hilo')
+    }
+
+
 }
--- steps/login.steps.ts
+++ steps/login.steps.ts
@@ -9,3 +9,7 @@ Then('I should see the title {string}', async (expectedTitle) => {
 Then('I will login as {string}', async (userName) => {
   await new Login(getPage()).loginAsUser(userName);
 });
+
+Then('verify error message {string}', async (expectedErrorMessage) => {
+  await new Login(getPage()).verifyLoginErrorMessage(expectedErrorMessage);
+});
