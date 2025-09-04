43e5dec58af6482afc52207b17536b800e4f2a31
Simanta1001 <103870362+Simanta1001@users.noreply.github.com>
2025-09-04 16:11:40 -0500

    assessment done

--- steps/common.steps.ts
+++ steps/common.steps.ts
@@ -1,6 +1,6 @@
 import { Given } from "@cucumber/cucumber";
-import { getPage } from "../playwrightUtilities";
 
-Given('I open the {string} page', async (url) => {
-    await getPage().goto(url);
-  });
+Given("I open the {string} page", async function (url: string) {
+  const page = (this as any).page;
+  await page.goto(url, { waitUntil: "domcontentloaded" });
+});
