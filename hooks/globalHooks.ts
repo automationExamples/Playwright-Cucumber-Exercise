8e4ebcac9c9a08462c6b8819d58f7a11b8333519
Alex <sasham2@yandex.ru>
2025-09-16 13:19:15 -0400

    delete log

--- hooks/globalHooks.ts
+++ hooks/globalHooks.ts
@@ -4,14 +4,10 @@ import { closeBrowser, initializeBrowser, initializePage } from "../playwrightUt
 setDefaultTimeout(15000);
 
 Before(async () => {
-  console.log("Running Before hook...");
   await initializeBrowser();
   await initializePage();
-  console.log("Browser and page initialized in Before hook.");
 });
 
 After(async () => {
-  console.log("Running After hook...");
   await closeBrowser();
-  console.log("Browser closed in After hook.");
 });
