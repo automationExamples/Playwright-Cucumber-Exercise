b9aca5be6765ce1d2594a87d900cdcf38e22a3dc
Kristina Drobava <galuskodrobova@gmail.com>
2025-10-07 16:24:12 -0400

    Validate product sort by price sort

--- steps/product.steps.ts
+++ steps/product.steps.ts
@@ -1,7 +1,29 @@
-import { Then } from '@cucumber/cucumber';
+import { Then, When } from '@cucumber/cucumber';
 import { getPage } from '../playwrightUtilities';
 import { Product } from '../pages/product.page';
+import assert from 'assert';
+
 
 Then('I will add the backpack to the cart', async () => {
   await new Product(getPage()).addBackPackToCart();
+});
+
+
+When('I sort the items by {string}', async (sortOption: string) => {
+  const product = new Product(getPage());
+  await product.sortProductsBy(sortOption);
+
+  // Ждем, пока первый товар обновится (можно ожидать любого индикатора)
+  await getPage().locator('.inventory_item_price').first().waitFor({ state: 'visible', timeout: 5000 });
+});
+
+
+Then('I validate all 6 items are sorted correctly by price {string}', async (direction: string) => {
+  const product = new Product(getPage());
+  const prices = await product.getProductPrices();
+
+  
+  const sortedPrices = [...prices].sort((a, b) => direction === 'ascending' ? a - b : b - a);
+
+  assert.deepStrictEqual(prices, sortedPrices, `Products are not sorted ${direction}`);
 });
