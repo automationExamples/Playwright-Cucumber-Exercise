92a8fc8a68fb30fdda33fe2b3b91963fd3c46741
Kristina Drobava <galuskodrobova@gmail.com>
2025-10-07 22:49:45 -0400

    Remove unnecessary wait and comment in  product.steps.ts

--- steps/product.steps.ts
+++ steps/product.steps.ts
@@ -12,17 +12,12 @@ Then('I will add the backpack to the cart', async () => {
 When('I sort the items by {string}', async (sortOption: string) => {
   const product = new Product(getPage());
   await product.sortProductsBy(sortOption);
-
-  // Ждем, пока первый товар обновится (можно ожидать любого индикатора)
-  await getPage().locator('.inventory_item_price').first().waitFor({ state: 'visible', timeout: 5000 });
 });
 
 
 Then('I validate all 6 items are sorted correctly by price {string}', async (direction: string) => {
   const product = new Product(getPage());
   const prices = await product.getProductPrices();
-
-  
   const sortedPrices = [...prices].sort((a, b) => direction === 'ascending' ? a - b : b - a);
 
   assert.deepStrictEqual(prices, sortedPrices, `Products are not sorted ${direction}`);
