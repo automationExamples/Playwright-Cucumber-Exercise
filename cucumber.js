2be05bca8d6ed5e112deab8d73ef48f8862ad559
Bhargavgade <bhargavb91@gmail.com>
2025-10-10 15:24:15 -0400

    Update cucumber.js

--- cucumber.js
+++ cucumber.js
@@ -1,3 +1,7 @@
 module.exports = {
-    default: `--require-module ts-node/register --require './steps/**/*.ts' --require './hooks/**/*.ts --format @cucumber/pretty-formatter`
+ default: `--require-module ts-node/register --require './steps/**/*.ts' --require './hooks/**/*.ts --format @cucumber/pretty-formatter`
 };
+
+
+
+
